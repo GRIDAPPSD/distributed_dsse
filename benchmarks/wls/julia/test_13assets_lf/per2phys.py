e46cb87c5080b2ed15a02ac86f281c2b416cebed
garydblack <gary.black@pnnl.gov>
2022-03-31 15:47:45 -0700

    13assets_lf test case for Julia weighted least squares state estimate formulation code.

---
+++ benchmarks/wls/julia/test_13assets_lf/per2phys.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import math
+
+def noground(x):
+  return x
+
+def ground(x):
+  return round(x, 2)
+
+# read vnom.csv into dictionary
+#Nodename,Mag,Arg
+#SOURCEBUS.1,66395.3,30.0
+#SOURCEBUS.2,66395.3,-90.0
+#SOURCEBUS.3,66395.3,150.0
+#671.1,2401.65,0.0
+#671.2,2401.63,-120.0
+#671.3,2401.61,120.0
+
+VnomMag = {}
+VnomArg = {}
+
+with open('vnom.csv') as vnomfile:
+  for line in vnomfile:
+    fields = line.split(',')
+    if fields[0] == 'Nodename':
+      continue
+    VnomMag[fields[0]] = float(fields[1])
+    VnomArg[fields[0]] = math.radians(float(fields[2]))
+#print(VnomMag)
+#print(VnomArg)
+
+# read and convert measurements.csv file
+#ztype,zid,znode1,znode2,zval-change,zsig-change,zpseudo,znomval-change
+#Pi,pseudo_P_671.1,671.1,671.1,-0.1925,0.194613,1,-0.1925
+#Qi,pseudo_Q_671.1,671.1,671.1,-0.11,0.111282,1,-0.11
+
+sbase = 1.0e+6
+idx = 1
+MeasType = {}
+MeasNode = {}
+
+newmeasfile = open('measurements.csv.new', 'w')
+
+with open('measurements.csv') as measfile:
+  for line in measfile:
+    fields = line.split(',')
+    if fields[0] == 'ztype':
+      newmeasfile.write(line)
+      continue
+
+    MeasType[idx] = fields[0]
+    MeasNode[idx] = fields[2]
+
+    if fields[0] == 'vi':
+      zval = noground(float(fields[4]) * VnomMag[fields[2]])
+      zsig = noground(float(fields[5]) * VnomMag[fields[2]])
+      znomval = noground(float(fields[7]) * VnomMag[fields[2]])
+    elif fields[0] == 'Ti':
+      #zval = noground(float(fields[4]))
+      #zsig = noground(float(fields[5]))
+      #znomval = noground(float(fields[7]))
+      zval = noground(float(fields[4]) + VnomArg[fields[2]])
+      zsig = noground(float(fields[5]) + VnomArg[fields[2]])
+      znomval = noground(float(fields[7]) + VnomArg[fields[2]])
+    else: # Pi and Qi
+      zval = noground(float(fields[4]) * sbase)
+      zsig = noground(float(fields[5]) * sbase)
+      znomval = noground(float(fields[7]) * sbase)
+
+    newmeasfile.write(fields[0]+','+fields[1]+','+fields[2]+','+fields[3]+','+str(zval)+','+str(zsig)+','+fields[6]+','+str(znomval)+'\n')
+
+    idx += 1
+
+newmeasfile.close()
+
+newdatafile = open('measurement_data.csv.new', 'w')
+
+with open('measurement_data.csv') as datafile:
+  for line in datafile:
+    fields = line.split(',')
+    if fields[0] == 'timestamp':
+      newdatafile.write(line)
+      continue
+
+    newdatafile.write(fields[0])
+
+    for idx in range(1, len(fields)):
+      if MeasType[idx] == 'vi':
+        zval = noground(float(fields[idx]) * VnomMag[MeasNode[idx]])
+      elif MeasType[idx] == 'Ti':
+        zval = noground(float(fields[idx]) + VnomArg[MeasNode[idx]])
+      else: # Pi or Qi
+        zval = noground(float(fields[idx]) * sbase)
+
+      newdatafile.write(','+str(zval))
+    newdatafile.write('\n')
+
+newdatafile.close()
+
+
